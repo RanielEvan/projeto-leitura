30e973515b1356493e54f273bd94cc51c0c27017
kellyane01 <devbrena@gmail.com>
2022-11-07 17:07:52 -0300

    Iniciando o form

--- src/pages/inicio/index.vue
+++ src/pages/inicio/index.vue
@@ -1,16 +1,37 @@
 <template>
   <q-page class="flex flex-center">
-    <h5>Bem vinda!</h5>
-    <!-- <<img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />> -->
+    <div class="row">
+      <div class="col-12"
+           v-for="(input) in inputs"
+           :key="'input-'+input.slug"
+      >
+        <q-input
+          v-if="input.tipo == 'text'"
+          rounded outlined
+          v-model="input.valor"
+          :label="input.label"
+          :placeholder="input.placeholder"
+        />
+
+        <q-input
+          v-if="input.tipo == 'text'"
+          rounded outlined
+          v-model="input.valor"
+          :label="input.label"
+          :placeholder="input.placeholder"
+        />
+      </div>
+    </div>
+<!--    <div class="q-pa-md">-->
+<!--      <div class="q-gutter-md" style="max-width: 300px">-->
+<!--    -->
+<!--      </div>-->
+<!--    </div>-->
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
 export default defineComponent({
   name: "PageIndex",
@@ -18,6 +39,28 @@ export default defineComponent({
   data() {
     return {
       hint: "Bem vindo ao Projeto Leitura!",
+      inputs: [
+        {
+          slug: "nome",
+          placeholder: "Nome",
+          label: "Digite seu nome",
+          icone: "mdi-pencil-outline",
+          tipo: "text",
+          valor: null,
+          erro: "",
+          obrigatorio: true,
+        },
+        {
+          slug: "idade",
+          placeholder: "Idade",
+          label: "Digite sua idade",
+          icone: "mdi-pencil-outline",
+          tipo: "number",
+          valor: null,
+          erro: "",
+          obrigatorio: true,
+        },
+      ],
     };
   },
 
